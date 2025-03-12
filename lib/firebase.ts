8dbab5d67d9468675a9a58cf6810f9009d8e60be
Childofnasdaq <164145300+Childofnasdaq@users.noreply.github.com>
2025-03-11 17:29:30 -0700

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore"
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.AIzaSyDE54BfEl0Qx7jqDnXeFPwy0nrDabAmi7U,
+  authDomain: process.env.quicktradepro-fbed4.firebaseapp.com,
+  projectId: process.env.quicktradepro-fbed4,
+  storageBucket: process.env.quicktradepro-fbed4.firebasestorage.app,
+  messagingSenderId: process.env.443810193511,
+  appId: process.env.1:443810193511:web:85c860d4b61ecd64cff4c2,
+  measurementId: process.env.G-EF3W2RMNRQ,
 }
 
 // Initialize Firebase
