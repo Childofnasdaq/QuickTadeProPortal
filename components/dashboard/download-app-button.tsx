8c398c5f24ccda7b033f27cbf274a3655ba27e36
Childofnasdaq <164145300+Childofnasdaq@users.noreply.github.com>
2025-03-12 06:07:37 -0700

    Create download-app-button.tsx

---
+++ components/dashboard/download-app-button.tsx
@@ -0,0 +1,10 @@
+// components/dashboard/download-app-button.tsx
+import React from "react";
+
+export const DownloadAppButton: React.FC = () => {
+  return (
+    <button className="bg-blue-500 text-white p-2 rounded">
+      Download App
+    </button>
+  );
+};
